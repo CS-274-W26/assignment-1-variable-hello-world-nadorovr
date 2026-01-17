70215af0ba748000bd0a499658e154d40d4a8292
Roman Nadorov <nadorovr@flip1.engr.oregonstate.edu>
2026-01-16 16:35:40 -0800

    Completed the assignment

--- prog1.c
+++ prog1.c
@@ -1,4 +1,6 @@
 #include <stdio.h> // Necessary for printf()
+#include <time.h>
+#include <stdlib.h>
 
 // TODO Other includes
 
@@ -8,4 +10,19 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+
+	srand(time(NULL));
+
+	int myrand = rand() % 11;
+
+	printf("%d\n", myrand);
+
+	if (myrand <= 4) {
+		printf("Eat more beef, kick less cats\n");
+	} else if (myrand == 10) {
+		printf("Larn is the best roguelike\n");
+	} else {
+		printf("FRODO LIVES\n"); 
+	}
+
 }
